166187d4defbdaa74238b5ffe0226299d1797b2d
Evan Jones <evan.q.jones@gmail.com>
2024-09-18 05:14:12 -0400

    wip

--- scripts/scene_rgbd.py
+++ scripts/scene_rgbd.py
@@ -1,7 +1,8 @@
 import argparse
-import time
-import shlex
+import os.path
 import readline
+import shlex
+import time
 
 from PIL import Image
 import numpy as np
@@ -35,6 +36,8 @@ fluxes = {
 depth_processor = AutoImageProcessor.from_pretrained("LiheYoung/depth-anything-large-hf")
 depth_model = AutoModelForDepthEstimation.from_pretrained("LiheYoung/depth-anything-large-hf")
 
+base_path = os.path.dirname(os.path.abspath(__file__))
+
 
 def predict_depth(image):
     inputs = depth_processor(images=image, return_tensors="pt")
@@ -49,8 +52,35 @@ def predict_depth(image):
         align_corners=False,
     )
 
-    return prediction.squeeze().cpu().numpy().astype('u1')
+    return Image.fromarray(prediction.squeeze().cpu().numpy().astype('u1'))
 
+
+def write_point_cloud(image, depth_im, path):
+    depth_scaling = 2
+    depth_values = np.array(depth_im).flatten() * (image.size[1] / 255) * depth_scaling
+    colors = np.array(image).reshape(-1, 3)
+    points = np.indices(image.size[::-1]).reshape(2, -1).T
+    points = np.c_[points, depth_values]
+
+    with open(path, "w") as ply_file:
+        ply_file.write("ply\n")
+        ply_file.write("format ascii 1.0\n")
+        ply_file.write(f"element vertex {len(points)}\n")
+        ply_file.write("property float x\n")
+        ply_file.write("property float y\n")
+        ply_file.write("property float z\n")
+        ply_file.write("property uchar red\n")
+        ply_file.write("property uchar green\n")
+        ply_file.write("property uchar blue\n")
+        ply_file.write("end_header\n")
+        for (y, x, z), (r, g, b) in zip(points, colors):
+            ply_file.write(f"{x} {SCR_HEIGHT - y} {z} {r} {g} {b}\n")
+
+histfile = os.path.join(base_path, '.history')
+try:
+    readline.read_history_file(histfile)
+except FileNotFoundError:
+    pass
 
 while (line := input('> ')):
     try:
@@ -60,6 +90,7 @@ while (line := input('> ')):
     prompt = line[:arg_idx].strip()
     if not prompt:
         continue
+    readline.write_history_file(histfile)
     args = parser.parse_args(shlex.split(line[arg_idx:] if arg_idx is not None else ''))
 
     steps = args.steps if args.steps is not None else 20 if args.model == 'dev' else 2
@@ -78,31 +109,12 @@ while (line := input('> ')):
         image = fluxes[args.model].generate_image(**flux_params).image
     except KeyboardInterrupt:
         continue
-      
-    image.show()
 
     depth_im = predict_depth(image)
 
-    Image.fromarray(depth_im.astype('u1')).show()
+    image = image.resize((800, 600))
+    depth_im = depth_im.resize((800, 600))
+    
+    write_point_cloud(image, depth_im, os.path.join(base_path, 'point_cloud.ply'))
 
-    # Convert depth image to point cloud and save as PLY
-    depth_values = depth_im.flatten()
-    colors = np.array(image).reshape(-1, 3)
-    points = np.indices((SCR_HEIGHT, SCR_WIDTH)).reshape(2, -1).T
-    points = np.c_[points, depth_values]
-
-    # Write to PLY file
-    with open("point_cloud.ply", "w") as ply_file:
-        ply_file.write("ply\n")
-        ply_file.write("format ascii 1.0\n")
-        ply_file.write(f"element vertex {len(points)}\n")
-        ply_file.write("property float x\n")
-        ply_file.write("property float y\n")
-        ply_file.write("property float z\n")
-        ply_file.write("property uchar red\n")
-        ply_file.write("property uchar green\n")
-        ply_file.write("property uchar blue\n")
-        ply_file.write("end_header\n")
-        for (x, y, z), (r, g, b) in zip(points, colors):
-            ply_file.write(f"{x} {y} {z} {r} {g} {b}\n")
 
